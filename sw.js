a6599431789692633f9e1551e773aec83972e320
delia-sismo <delia@igeofisica.unam.mx>
2025-11-19 23:31:19 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,33 @@
+// sw.js
+const CACHE_NAME = "magnitud-ml-v2";  // <- cambia el nombre de la versión
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./ARIG.jpg",
+  "./CMIG.jpg",
+  "./MOIG.jpg"
+];
 
-const CACHE_NAME = "magnitud-ml-v1";
-const URLS_TO_CACHE = ["./","./index.html","./manifest.json","./ARIG.jpg","./CMIG.jpg","./MOIG.jpg"];
-
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS_TO_CACHE)));
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
+
+self.addEventListener("activate", e => {
+  // borra cachés viejas
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
 });
